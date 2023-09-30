bb19c5d8f37f1bcbebbcf870cf4f9f2533f54bf8
saifion33 <mxofficialplayer@gmail.com>
2023-09-30 10:27:28 +0530

    lazy load 2 component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Sidebar from "./components/sidebar/Sidebar"
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer} from "react-toastify"
-import Leaderboard from "./pages/Leaderboard"
-import SavedStory from "./pages/SavedStory"
+// import Leaderboard from "./pages/Leaderboard"
+// import SavedStory from "./pages/SavedStory"
 import Navbar from "./components/Navbar"
 import Signup from "./pages/Signup"
+import {Suspense,lazy} from 'react'
 import Login from "./pages/Login"
 import Story from './pages/Story'
 import { useState } from "react"
 import Home from "./pages/Home"
+import Loading from './components/Loading'
+const SavedStory =lazy(()=>import('./pages/SavedStory'))
+const Leaderboard =lazy(()=>import('./pages/Leaderboard'))
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,8 +26,8 @@ function App() {
           <div className="h-screen overflow-y-auto pb-12 w-full" >
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/saved" element={<SavedStory />} />
-              <Route path="/leaderboard" element={<Leaderboard />} />
+              <Route path="/saved" element={<Suspense fallback={<Loading/>} ><SavedStory /></Suspense>} />
+              <Route path="/leaderboard" element={<Suspense fallback={<Loading/>} ><Leaderboard /></Suspense>} />
               <Route path="/auth/signup" element={<Signup/>} />
               <Route path="/auth/login" element={<Login/>} />
               <Route path='/story/:storyId' element={<Story/>}/>
